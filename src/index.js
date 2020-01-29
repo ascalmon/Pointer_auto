e57426bf3fe16defcace953c615c140633dab023
ascalmon <ascalmon@gmail.com>
2020-01-29 20:22:53 -0300

    Sync

--- src/index.js
+++ src/index.js
@@ -1,10 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-
 import App from './App';
 import * as serviceWorker from './serviceWorker';
-import '../node_modules/font-awesome/css/font-awesome.min.css';
+
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
