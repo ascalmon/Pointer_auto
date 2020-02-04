b1513791bcae3ef6102cb027a64467d148a0760e
ascalmon <ascalmon@gmail.com>
2020-02-03 23:51:13 -0300

    Change Contact CSS size and color of letters.

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -33,28 +33,28 @@ class Contact extends Component {
                 <div id="form">
                   <form id="waterform" method="get">
                     <div className="formgroup" id="name-form">
-                      <label htmlFor="name">{i18n.t(k.YOUR_NAME)}</label>
+                      <label className="contact-label" htmlFor="name">{i18n.t(k.YOUR_NAME)}</label>
                       <input class="contact-input" type="text" id="name" name="name" />
                     </div>
                     <div className="formgroup" id="email-form">
-                      <label htmlFor="email">{i18n.t(k.YOUR_E_MAIL)}</label>
+                      <label className="contact-label" htmlFor="email">{i18n.t(k.YOUR_E_MAIL)}</label>
                       <input class="contact-input" type="email" id="email" name="email" />
                     </div>
                     <div className="formgroup" id="selection-form">
                       <div class="form-group">
-                        <label for="sel1">{i18n.t(k.REASON_FOR_YOUR_CONTACT)}</label>
+                        <label className="contact-label" for="sel1">{i18n.t(k.REASON_FOR_YOUR_CONTACT)}</label>
 
                         <select class="form-control contact-input" id="sel1">
                           <option className="contact-input" active></option>
                           <option className="contact-input">Quotation</option>
-                          <option>New Project</option>
-                          <option>Complain</option>
-                          <option>Technical Support</option>
+                          <option className="contact-input">New Project</option>
+                          <option className="contact-input">Complain</option>
+                          <option className="contact-input">Technical Support</option>
                         </select>
                       </div>
                     </div>
                     <div className="formgroup" id="message-form">
-                      <label htmlFor="message">{i18n.t(k.YOUR_MESSAGE)}</label>
+                      <label className="contact-label" htmlFor="message">{i18n.t(k.YOUR_MESSAGE)}</label>
                       <textarea id="message" name="message"></textarea>
                     </div>
                   	<input type="submit" value={i18n.t(k.SEND_MESSAGE)} />
